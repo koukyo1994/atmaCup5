a3837b567b4a7f62bbd2ba9ee245a49c6997115b
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-05-20 23:14:20 +0900

    add concatenate feature callback

--- src/core/callbacks/features.py
+++ src/core/callbacks/features.py
@@ -1,6 +1,11 @@
+import gc
+
 import pandas as pd
 
+import src.utils as utils
+
 from scipy.io import savemat
+from scipy.sparse import csr_matrix, hstack
 
 from src.core.states import RunningState
 
@@ -20,7 +25,57 @@ class FeatureSavingCallback(Callback):
             for phase in ["train", "test"]:
                 if isinstance(feature[phase], dict):
                     mdict = {name: feature[phase]}
-                    savemat(feature_dir / f"{name}_{phase}.mat", mdict)
+                    with utils.timer("Saving " + f"{name}_{phase}.mat",
+                                     state.logger):
+                        savemat(feature_dir / f"{name}_{phase}.mat", mdict)
                 elif isinstance(feature[phase], pd.DataFrame):
-                    feature[phase].to_feather(
-                        feature_dir / f"{name}_{phase}.ftr")
+                    with utils.timer("Saving " + f"{name}_{phase}.ftr",
+                                     state.logger):
+                        feature[phase].to_feather(
+                            feature_dir / f"{name}_{phase}.ftr")
+                else:
+                    raise NotImplementedError
+
+
+class ConcatenateFeatureCallback(Callback):
+    signature = "features"
+    callback_order = CallbackOrder.HIGHEST
+
+    def on_features_end(self, state: RunningState):
+        features = state.features
+
+        as_sparse = False
+        for feature in features.values():
+            if isinstance(feature["train"], dict):
+                as_sparse = True
+                break
+
+        main_feature = {}
+        with utils.timer("Concatnating `main` features", state.logger):
+            if as_sparse:
+                for phase in ["train", "test"]:
+                    sparse_matrices = []
+                    for f in features.values():
+                        if isinstance(f[phase], pd.DataFrame):
+                            feature_values = csr_matrix(f[phase].values)
+                            sparse_matrices.append(feature_values)
+                        elif isinstance(f[phase], dict):
+                            sparse_dict = f[phase]
+                            for sp_mat in sparse_dict.values():
+                                sparse_matrices.append(sp_mat)
+                    main_feature[phase] = {"main": hstack(sparse_matrices)}
+            else:
+                for phase in ["train", "test"]:
+                    dfs = []
+                    for f in features.values():
+                        dfs.append(f[phase])
+
+                    main_feature[phase] = pd.concat(dfs, axis=1)
+        state.features["main"] = main_feature
+
+        keys = list(features.keys())
+        keys.remove("main")
+
+        for key in keys:
+            del state.features[key]
+            gc.collect()
