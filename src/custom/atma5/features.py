e8e2dfc6cd10c0d8d1e999bbf0a544acdf02df9f
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-05-31 16:28:56 +0900

    add skew kurtなどなど

--- src/custom/atma5/features.py
+++ src/custom/atma5/features.py
@@ -313,3 +313,29 @@ class SpecMaxThreshed:
 
         df = pd.DataFrame(result_dict)
         return df
+
+
+class SpecStdSkewKurt:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def fit_transform(self, X: pd.DataFrame):
+        return self.transform(X)
+
+    def transform(self, X: pd.DataFrame):
+        operations = ["std", "skew", "kurt"]
+        prefix = self.kwargs["prefix"] + "_"
+        result_dict: Dict[str, List[Union[int, float]]] = {
+            op: []
+            for op in operations
+        }
+
+        base_dir = Path("input/atma5/spectrum")
+        for _, row in progress_bar(X.iterrows(), total=len(X)):
+            spectrum = pd.read_csv(
+                base_dir / row.spectrum_filename, sep="\t", header=None)
+            for op in operations:
+                result_dict[op].append(spectrum[1].__getattribute__(op)())
+        df = pd.DataFrame(result_dict)
+        df.columns = [prefix + c for c in df.columns]
+        return df
