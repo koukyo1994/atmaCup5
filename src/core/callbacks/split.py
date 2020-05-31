9acff89c7b78025a8aec34525ab43451b2683c52
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-05-31 12:41:11 +0900

    10 failed

--- src/core/callbacks/split.py
+++ src/core/callbacks/split.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-from scipy.sparse import csr_matrix
+import src.utils as utils
+
+from scipy.sparse import csr_matrix, hstack
 
 from src.core.states import RunningState
 from .base import Callback, CallbackOrder
@@ -42,3 +44,40 @@ class PrepareForStratifiedKFoldCallback(Callback):
         train = state.features["main"]["train"]
         train = train.drop("y", axis=1)
         state.features["main"]["train"] = train
+
+
+class ConcatenateFeatureCallback(Callback):
+    signature = "split"
+    callback_type = CallbackOrder.HIGHEST
+
+    def on_split_start(self, state: RunningState):
+        features = state.features
+
+        as_sparse = False
+        for feature in features.values():
+            if isinstance(feature["train"], dict):
+                as_sparse = True
+                break
+
+        main_feature = {}
+        with utils.timer("Concatnating `main` features", state.logger):
+            if as_sparse:
+                for phase in ["train", "test"]:
+                    sparse_matrices = []
+                    for f in features.values():
+                        if isinstance(f[phase], pd.DataFrame):
+                            feature_values = csr_matrix(f[phase].values)
+                            sparse_matrices.append(feature_values)
+                        elif isinstance(f[phase], dict):
+                            sparse_dict = f[phase]
+                            for sp_mat in sparse_dict.values():
+                                sparse_matrices.append(sp_mat)
+                    main_feature[phase] = hstack(sparse_matrices).tocsr()
+            else:
+                for phase in ["train", "test"]:
+                    dfs = []
+                    for f in features.values():
+                        dfs.append(f[phase])
+
+                    main_feature[phase] = pd.concat(dfs, axis=1)
+        state.features["main"] = main_feature
